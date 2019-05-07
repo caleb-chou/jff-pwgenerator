05dc387cb8e5fdf7544a6986e75a405a0523f428
Caleb <haloisawesome412@gmail.com>
2019-05-07 09:18:11 -0500

    Added new class + variables

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 class Main {
   
 }
+class Password {
+    private ArrayList<char[]> char_set = new ArrayList<char[]>();
+
+    private String lower = "abcdefghijklmnopqrstuvwxyz";
+    private String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String nums  = "0123456789";
+    private String special="!@#$%^&*()-=_+;':\"[]{},./<>?\\";
+}
