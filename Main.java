c519255a20ac46017152dc75f8770c388bb1f7ea
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-05-12 13:46:06 -0500

    Update Code

--- Main.java
+++ Main.java
@@ -17,6 +17,7 @@ class Password {
         if(u) char_set.add(upper.toCharArray());
         if(n) char_set.add(nums.toCharArray());
         if(s) char_set.add(special.toCharArray());
+        generate(len);
 
     }
     private int random_num(int range) {
@@ -25,5 +26,13 @@ class Password {
 
     public String toString() {
         return password;
+    }
+    
+    private void generate(int len) {
+        int i = 0;
+        while( i++ < len ) {
+          int cat = random_num(char_set.size());
+          this.password += "" + this.char_set.get(cat)[random_num(this.char_set.get(cat).length)];
+        }
     }
 }
